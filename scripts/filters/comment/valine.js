e64907aa022bf375aaca4cfd76643e83bff6e795
lchen <lchen@finbtc.net>
2020-03-02 09:44:56 +0800

    remove valine static in post-meta

--- scripts/filters/comment/valine.js
+++ scripts/filters/comment/valine.js
@@ -17,19 +17,3 @@ hexo.extend.filter.register('theme_inject', injects => {
 });
 
 // Add post_meta
-hexo.extend.filter.register('theme_inject', injects => {
-  let theme = hexo.theme.config;
-  if (!theme.valine.enable || !theme.valine.appid || !theme.valine.appkey) return;
-
-  injects.postMeta.raw('valine', `
-  {% if post.comments and (is_post() or theme.valine.comment_count) %}
-  <span class="post-meta-item">
-    ${iconText('comment-o', 'valine')}
-    <a title="valine" href="{{ url_for(post.path) }}#valine-comments" itemprop="discussionUrl">
-      <span class="post-comments-count valine-comment-count" data-xid="{{ url_for(post.path) }}" itemprop="commentCount"></span>
-    </a>
-  </span>
-  {% endif %}
-  `, {}, {}, theme.valine.post_meta_order);
-
-});
